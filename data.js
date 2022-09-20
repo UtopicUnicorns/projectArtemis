558166b2317650b51e5313a6ead4f3b6e8e67de3
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2022-09-20 09:54:36 +0200

    del
    del

--- data.js
+++
@@ -1,1 +0,0 @@
-let os_data = {"guild_count":3,"time":"09:55:41","date":"2022/6/1","os_cpu":"Intel(R) Core(TM) i7-4790 CPU @ 3.60GHz","os_cpuavg":[0.58,0.45,0.43],"os_plat":"linux","os_rel":"5.18.1-arch1-1","uptime":{"hour":"00","minute":"00","second":"17"},"os_memtotal":15947,"os_memfree":12141,"os_meminuse":3806,"os_up":{"hour":"01","minute":"45","second":"46"},"bot_mem":8}
