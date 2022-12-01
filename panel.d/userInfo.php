005c6384a3dcd84a920488661e2d3dd953a74c72
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2022-12-01 15:46:40 +0100

    Spaghetti
    SpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghettiSpaghetti

--- panel.d/userInfo.php
+++ panel.d/userInfo.php
@@ -4,13 +4,28 @@
     
   if($yourInformation->banner) $userBanner = 'https://cdn.discordapp.com/banners/' . $yourInformation->id . '/' . $yourInformation->banner . '.png?size=2048';
   if(!$yourInformation->banner) $userBanner = './images/backgrounds/2.png';
+  
   $personalConnection = new mysqli('localhost', $sqlUser, $sqlPass);
+  $getDb = $personalConnection->query("SHOW DATABASES LIKE 'g%';");
+  $gatherEverything = [];
+  foreach($getDb as $parseDb) {
+    $dbName = $parseDb["Database (g%)"];
+    $chanPull = $personalConnection->query("SHOW TABLES FROM " . $dbName . " LIKE 'c%';");
+    foreach($chanPull as $chansOut) {
+      $tableName = $chansOut["Tables_in_{$dbName} (c%)"];
+      $gatherData = $personalConnection->query("USE ${dbName};");
+      $gatherMoreData = $personalConnection->query("SELECT value FROM {$tableName} WHERE id = {$yourInformation->id};")->fetch_object()->value;
+      if($gatherMoreData) $gatherEverything[substr($tableName, 1)] = array($gatherMoreData, substr($dbName, 1)); 
+    }
+  }
+  $moreCon = json_encode($gatherEverything);
+  
   $siteContext = '<div style="width: 100%; height: 25%; background-size: cover; background-repeat: no-repeat; background-position: center; background-image: url(\'' . $userBanner . '\'); display: flex; place-items: center left;">
                     <img src="' . $userAvatar . '" style="height: 25vh; border-radius: 500px; margin-left: 50px; box-shadow: 5px 5px 2px black;" /><h1 style="margin-left: 20px;" id="nameInUserPanel"> ' . $yourInformation->username . '#' . $yourInformation->discriminator . '<br />' . $yourInformation->id . '</h1>
                   </div>
                   <div style="width: 100%; height: 65%; background-color: rgba(255, 255, 255, 0.5); display: flex; place-items: center;">
                     <div style="width: 100%; height: 100%; overflow: auto;text-align: center; display: grid; place-items: center;">
-                      ffff
+                      '.$moreCon.'
                     </div>
                   </div>
                   <div style="width: 100%; height: 10%; background-size: cover; background-repeat: no-repeat; background-position: center; background-image: url(\'./images/assets/project_artemis_background.svg\'); background-color: rgba(255, 255, 255, 0.7); display: grid; place-items: center;">
