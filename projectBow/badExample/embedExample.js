aad64bdaab0411800997b23b264c6e22854be642
initrd <30652988+UtopicUnicorns@users.noreply.github.com>
2024-03-26 11:52:11 +0100

    Update embedExample.js

--- projectBow/badExample/embedExample.js
+++ projectBow/badExample/embedExample.js
@@ -10,7 +10,68 @@
     so here is a small example.
     This file will not show you how to send the embed, but rather how to create it.
     Each embed should be started with a 'new embed()', this gives you a blank slate to work with.
-    
+    Limitations and usage:
+      .title()
+        Singular string with a maximum of 256 characters.
+        
+      .description()
+        Singular string, supports newlines maximum of 4096 characters.
+      
+      .url()
+        Singular string.
+      
+      .color()
+        HEX color without '#' as a string.
+        
+      .field()
+        Field needs a JSON object such as {}
+        object entry 'name' has a maximum of 256 characters.
+        object entry 'value' has a maximum of 1024 characters.
+        object entry 'inline' is a boolean.
+        There is a maximum of 25 fields per embed.
+        
+      .author()
+        Author needs a JSON object such as {}
+        Object entry 'name' has a maximum of 256 characters.
+        Object entry 'url' 
+        Object entry 'iconUrl' 
+        Object entry 'proxyIconUrl' 
+        
+      .image()
+        Image needs a JSON object such as {}
+        Object entry '' 
+        Object entry '' 
+        Object entry '' 
+        Object entry '' 
+        
+      .thumbnail()
+        Thumbnail needs a JSON object such as {}
+        Object entry '' 
+        Object entry '' 
+        Object entry '' 
+        Object entry '' 
+        
+      .video()
+        Video needs a JSON object such as {}
+        Object entry '' 
+        Object entry '' 
+        Object entry '' 
+        Object entry '' 
+        
+      .provider()
+        Provider needs a JSON object such as {}
+        Object entry '' 
+        Object entry '' 
+        
+      .footer()
+        Footer needs a JSON object such as {}
+        Object entry 'text' has a maximum of 2048 characters. 
+        Object entry '' 
+        Object entry '' 
+      
+      .timestamp()
+        ISO8601 timestamp or leave empty for current time.
+  
 */
 new embed()
   .title()
